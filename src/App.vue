5736407830f441c1dfb51e7a57336e276ffa5b05
manuJRS <ma_nuel_rs@hotmail.com>
2026-02-05 21:28:36 -0600

    feat(usecart): :art: added cart icon & functions to hydrate from app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
 import MainLayout from '@/layouts/MainLayout.vue'
+import { useCartStore } from '@/stores/cartStore'
+
+const cart = useCartStore()
+
+onMounted(() => {
+  cart.hydrate()
+})
 </script>
 
 <template>
